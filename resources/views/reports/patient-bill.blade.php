ddd5602f14404ada1af287058afcc3e5326111c4
Erik <erik.henry@dcmail.ca>
2018-12-03 15:36:02 -0500

    testing

--- resources/views/reports/patient-bill.blade.php
+++ resources/views/reports/patient-bill.blade.php
@@ -6,14 +6,17 @@
 <table><tr>
 <td width="100%" align="center">
 <h1 class="title">Dale Robertson</h1>
-</td>
-<td width="100%" align=center">
-<h2 class="title2">Licensed Psychologist</h2>
-</td></tr></table>
+</td></tr>
+<tr>
+<td width="100%" align="center">
+<h3 class="title2">Licensed Psychologist</h3>
+</td></tr>
 
-
-56 So. Meadow Road  (518) 746-4242
-Columbus, OH 02340  DR@email.net
+<tr><td width="50%" align="left">56 So. Meadow Road</td>
+    <td width="50%" align "right">(518) 746-4242</td></tr>
+<tr><td width="50%" align="left">Columbus, OH 02340</td>
+    <td width="50%" align="right">DR@email.net</td></tr>
+</table>
 <br>
 <h4 class="right">For Professional Services To:</h4>
 <hr/>
