5f70403dfc10fbfe6da84436730db6184a84cca0
Erik <erik.henry@dcmail.ca>
2018-12-03 16:02:41 -0500

    testing

--- resources/views/reports/patient-bill.blade.php
+++ resources/views/reports/patient-bill.blade.php
@@ -3,45 +3,67 @@
 @section ('content')
 
 <!--PUT STUFF HERE-->
-<table><tr>
-<td width="100%" align="center">
-<h1 class="title">Dale Robertson</h1>
-</td></tr>
-<tr>
-<td width="100%" align="center">
-<h3 class="title2">Licensed Psychologist</h3>
-</td></tr>
+<table>
+	<tr>
+		<td width="100%" align="center">
+			<h1 class="title">Dale Robertson</h1>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%" align="center">
+			<h3 class="title2">Licensed Psychologist</h3>
+		</td>
+	</tr>
 
-<tr><td width="30%" align="center">56 So. Meadow Road</td>
-    <td width="50%" align="center">(518) 746-4242</td></tr>
-<tr><td width="30%" align="center">Columbus, OH 02340</td>
-    <td width="50%" align="center">DR@email.net</td></tr>
+	<tr>
+		<td width="30%" align="left">56 So. Meadow Road</td>
+    	<td width="30%" align="left">(518) 746-4242</td></tr>
+	<tr>
+		<td width="30%" align="left">Columbus, OH 02340</td>
+    	<td width="30%" align="left">DR@email.net</td>
+    </tr>
 </table>
+
 <br>
 
-<table><tr>
-	<td width="80%" align="right">
-<h4 class="title3">For Professional Services To:</h4>
-	</td></tr></table>
+<table>
+	<tr>
+		<td width="60%" align="right">
+			<title3>For Professional Services To:</title3>
+		</td>
+	</tr>
+</table>
+
 <hr/>
 
 <table>
 	<tr>
-		<td>Date</td>
-		<td>Service</td>
-		<td>Hours/Units</td>
-		<td>Rate</td>
-		<td>Charge</td>
-		<td>Patient Payment</td>
-		<td>Date</td>
-		<td>Insurance Payment</td>
-		<td>Insurnace Adjustment</td>
-		<td>Date</td>
-		<td>Other Insur Payment</td>
-		<td>Other Insur Adjustment</td>
-		<td>Date</td>
-		<td>Other Adjustment</td>
-		<td>Balance Due</td>
+		<td width="70%" align="right">Isabelle R Goodman</td>
+		<td width="70%" align="left">Kenny Goodman</td>
+		<td width="70%" align="left">8 Village Landing</td>
+		<td width="70%" align="left">Columbus, OH 02046</td>
+	</tr>
+</table>
+
+<hr/>
+
+<table>
+	<tr>
+		<td width="5%">Date</td>
+		<td width="5%">Service</td>
+		<td width="10%">Hours/Units</td>
+		<td width="5%">Rate</td>
+		<td width="5%">Charge</td>
+		<td width="10%">Patient Payment</td>
+		<td width="5%">Date</td>
+		<td width="10%">Insurance Payment</td>
+		<td width="10%">Insurnace Adjustment</td>
+		<td width="5%">Date</td>
+		<td width="10%">Other Insur Payment</td>
+		<td width="10%">Other Insur Adjustment</td>
+		<td width="5%">Date</td>
+		<td width="5%">Other Adjustment</td>
+		<td width="5%">Balance Due</td>
 	</tr>
 </table>
 
