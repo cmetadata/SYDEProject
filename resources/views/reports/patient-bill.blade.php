0dca57f4bb399fb1eeea2382739d97ab07c70594
Erik <erik.henry@dcmail.ca>
2018-12-03 15:49:57 -0500

    testing

--- resources/views/reports/patient-bill.blade.php
+++ resources/views/reports/patient-bill.blade.php
@@ -12,13 +12,37 @@
 <h3 class="title2">Licensed Psychologist</h3>
 </td></tr>
 
-<tr><td width="50%" align="center">56 So. Meadow Road</td>
+<tr><td width="30%" align="center">56 So. Meadow Road</td>
     <td width="50%" align="center">(518) 746-4242</td></tr>
-<tr><td width="50%" align="center">Columbus, OH 02340</td>
+<tr><td width="30%" align="center">Columbus, OH 02340</td>
     <td width="50%" align="center">DR@email.net</td></tr>
 </table>
 <br>
+
+<table><tr>
+	<td width="80%" align="right">
 <h4 class="title3">For Professional Services To:</h4>
+	</td></tr></table>
 <hr/>
+
+<table>
+	<tr>
+		<td>Date</td>
+		<td>Service</td>
+		<td>Hours/Units</td>
+		<td>Rate</td>
+		<td>Charge</td>
+		<td>Patient Payment</td>
+		<td>Date</td>
+		<td>Insurance Payment</td>
+		<td>Insurnace Adjustment</td>
+		<td>Date</td>
+		<td>Other Insur Payment</td>
+		<td>Other Insur Adjustment</td>
+		<td>Date</td>
+		<td>Other Adjustment</td>
+		<td>Balance Due</td>
+	</tr>
+</table>
 
 @endsection
