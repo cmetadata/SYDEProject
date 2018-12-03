6213cdd8a7c83f29e7d7f626db7a99e6221b2d1b
Erik <erik.henry@dcmail.ca>
2018-12-03 15:42:02 -0500

    testing

--- resources/views/reports/patient-bill.blade.php
+++ resources/views/reports/patient-bill.blade.php
@@ -12,13 +12,13 @@
 <h3 class="title2">Licensed Psychologist</h3>
 </td></tr>
 
-<tr><td width="50%" align="left">56 So. Meadow Road</td>
-    <td width="50%" align "right">(518) 746-4242</td></tr>
-<tr><td width="50%" align="left">Columbus, OH 02340</td>
-    <td width="50%" align="right">DR@email.net</td></tr>
+<tr><td width="50%" align="center">56 So. Meadow Road</td>
+    <td width="50%" align="center">(518) 746-4242</td></tr>
+<tr><td width="50%" align="center">Columbus, OH 02340</td>
+    <td width="50%" align="center">DR@email.net</td></tr>
 </table>
 <br>
-<h4 class="right">For Professional Services To:</h4>
+<h4 class="title3">For Professional Services To:</h4>
 <hr/>
 
 @endsection
